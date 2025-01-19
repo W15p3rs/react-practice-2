c6434c794343238b0a872fd59ceacaf296193573
W15p3rs <a.pdlyak@gmail.com>
2025-01-19 12:26:19 +0000

    Added favourite cars

--- react-practice-2/src/MyComponent.jsx
+++ react-practice-2/src/MyComponent.jsx
@@ -1,23 +1,33 @@
-import React,{useState} from 'react';
-function MyComponent(){
+import React, { useState } from 'react';
+
+function MyComponent() {
 
 
-    const [count, setCount] = useState(0);
+    const [car, setCar] = useState({
+                                    year:2024,
+                                     make:"Chevrolet",
+                                     model:"Camaro"});
 
-    function increment(){
-        setCount(count+1);
+    
+    function handleYearChange(event){
+        setCar(c => ({...c, year: event.target.value}));
     }
-    function decrement(){
-        setCount(count-1);
+    function handleMakeChange(event){
+        setCar(c =>({...c, make: event.target.value}));
     }
-    function reset(){
-        setCount(0);
+    function handleModelChange(event){
+        setCar(c =>({...c, model: event.target.value}));
     }
 
-    return(
-        <div>
-            <p>HelloWorld</p>
-        </div>
-    )
+    return(<>
+    <div>
+        <p>Your favourite car is {car.year} {car.make} {car.model}</p>
+
+        <input type='number' value={car.year} onChange={handleYearChange}/> <br/>
+        <input type='text' value={car.make} onChange={handleMakeChange}/> <br/>
+        <input type='text' value={car.model} onChange={handleModelChange}/>
+    </div>
+    
+    </>)
 }
 export default MyComponent;
