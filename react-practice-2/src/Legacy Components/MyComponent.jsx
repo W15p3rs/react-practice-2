bf13ae5233d0fa4688c0d03028df85560570b036
W15p3rs <a.pdlyak@gmail.com>
2025-01-19 12:06:09 +0000

    Moved file to legacy components(updater functions)

---
+++ react-practice-2/src/Legacy Components/MyComponent.jsx
@@ -0,0 +1,35 @@
+import React,{useState} from 'react';
+function MyComponent(){
+
+
+    const [count, setCount] = useState(0);
+
+    function increment(){
+
+
+        setCount(c => c+1);
+        setCount(c => c+1);
+        setCount(c => c+1);
+        
+    
+    }
+
+    function decrement(){
+        setCount(c => c-1);
+        setCount(c => c-1);
+        setCount(c => c-1);
+    }
+    function reset(){
+        setCount(0);
+    }
+
+    return(
+        <div>
+            <h1>Count:{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button> 
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
+export default MyComponent;
